9aaab2bab00754329ed1d5d1ebc7483476750455
sammyrey6 <121529677+sammyrey6@users.noreply.github.com>
2023-08-01 13:36:43 -0700

    created config file (starting backend)

---
+++ Main/config/connection.js
@@ -0,0 +1,22 @@
+const Sequelize = require ('sequilize');
+require('dotenv').config
+
+let sequilize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: 'mysql',
+      port: 3306,
+      logging: false
+    }
+  );
+}
+
+module.exports = sequelize;
